09c50b27e812ebb0e6d1e3abf36e72111a3d43ff
jrlabanza <jrlabanza@gmail.com>
2020-05-29 15:19:51 +0800

    API Implementation

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+const uuid = require('uuid');
 const mysql = require('mysql');
 
 const app = express();
 
-const SELECT_ALL_PRODUCTS_QUERY = 'SELECT * FROM osr';
+const SELECT_ALL_PRODUCTS_QUERY = 'SELECT * FROM sample_table';
 
 const connection = mysql.createConnection({
-  host: 'phsm01ws012',
-  user: 'fthrdr',
-  password: 'fthrdr01',
-  database: 'ftdr'
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'reactdb'
 });
 
 connection.connect(err =>{
@@ -23,6 +24,19 @@ app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('go to /products to see products');
+});
+
+app.get('/products/add', (req, res) =>{
+  const { handlerID, testerID, description, remarks } = req.query;
+  const INSERT_PRODUCTS_QUERY = `INSERT INTO sample_table(id, handlerID, testerID, description, remarks) VALUE ('${uuid()}', ${handlerID}, '${testerID}', '${description}', '${remarks}')`;
+  connection.query(INSERT_PRODUCTS_QUERY, (err, results) =>{
+    if(err) {
+      return res.send(err)
+    }
+    else{
+      return res.send('successfully added product')
+    }
+  });
 });
 
 app.get('/products', (req, res) =>{
